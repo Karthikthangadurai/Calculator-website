f8318f2f67c3926f446e537ac1071e5a059fdfdf
Karthikthangadurai <presikarthik@gmail.com>
2026-09-20 09:36:02 +0530

    js functionality added

---
+++ script.js
@@ -0,0 +1,33 @@
+// To get element by Id's in HTML
+
+let inputData = document.getElementById("inputdata");
+let btnClear = document.getElementById("btnclear");
+let btnPct = document.getElementById("btnpct");
+let btnDel = document.getElementById("btndel");
+let btnDivid = document.getElementById("btndivid");
+let btnSeven = document.getElementById("btnseven");
+let btnEight = document.getElementById("btneight");
+let btnNine = document.getElementById("btnnine");
+let btnMulti = document.getElementById("btnmulti");
+let btnFour = document.getElementById("btnfour");
+let btnFive = document.getElementById("btnfive");
+let btnSix = document.getElementById("btnsix");
+let btnMinus = document.getElementById("btnminus");
+let btnOne = document.getElementById("btnone");
+let btnTwo = document.getElementById("btntwo");
+let btnThree = document.getElementById("btnthree");
+let btnPlus = document.getElementById("btnplus");
+let btnDzero = document.getElementById("btndzero");
+let btnZero = document.getElementById("btnzero");
+let btnDot = document.getElementById("btndot");
+let btnEqual = document.getElementById("btnequal");
+
+// InputData ku vara datas store panna pora
+
+
+
+// Button Seven Call panra
+btnSeven.addEventListener("click", function () {
+  inputData.value += "7";
+  
+});
